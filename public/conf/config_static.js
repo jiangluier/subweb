c966e8b67097996958c5759c2f94fe6ce7808a7f
jiangluier <jiangluier318@gmail.com>
2026-05-29 10:26:05 +0800

    Update config_static.js

--- public/conf/config_static.js
+++ public/conf/config_static.js
@@ -4,12 +4,16 @@ window.config = {
   // 后端 API 列表
   apiBackends: [
     {
-      name: '本地服务',
-      url: 'http://127.0.0.1:25500',
+      name: 'Aethersailor 后端',
+      url: 'https://api.asailor.org',
     },
     {
       name: '官方服务',
       url: 'https://sub.xeton.dev',
+    },
+    {
+      name: '肥羊增强型后端',
+      url: 'https://api.v1.mk',
     },
   ],
   // 是否启用短链接功能 (true: 启用, false: 关闭)
@@ -25,7 +29,7 @@ window.config = {
     },
     {
       title: 'GitHub',
-      link: 'https://github.com/Aethersailor/subweb',
+      link: 'https://github.com/jiangluier/subweb',
       target: '_blank',
     },
   ],
@@ -38,6 +42,22 @@ window.config = {
     {
       value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full.ini',
       text: 'ACL4SSR Online Full',
+    },
+    {
+      value: 'https://raw.githubusercontent.com/Aethersailor/Custom_OpenClash_Rules/refs/heads/main/cfg/Custom_Clash.ini',
+      text: 'Aethersailor 规则 标准版',
+    },
+    {
+      value: 'https://raw.githubusercontent.com/Aethersailor/Custom_OpenClash_Rules/refs/heads/main/cfg/Custom_Clash_Lite.ini',
+      text: 'Aethersailor 规则 轻量版',
+    },
+    {
+      value: 'https://raw.githubusercontent.com/Aethersailor/Custom_OpenClash_Rules/refs/heads/main/cfg/Custom_Clash_GFW.ini',
+      text: 'Aethersailor 规则 极简版(GFW)',
+    },
+    {
+      value: 'https://raw.githubusercontent.com/Aethersailor/Custom_OpenClash_Rules/refs/heads/main/cfg/Custom_Clash_Full.ini',
+      text: 'Aethersailor 规则 重度分流版',
     },
   ],
 };
